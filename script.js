f3bf7bbfae21f8f73296a191a1a087563155575b
AlinaZah31 <alina_shuvi@yahoo.com>
2024-06-11 18:01:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function generateButtons() {
-    const n = noButtons.value;
-    const randomBtn = Math.floor(Math.random() * n) + 1;
-    for (let i = 1; i <= n; i++) {
+    const genButtons = noButtons.value;
+    const randomBtn = Math.floor(Math.random() * genButtons) + 1;
+    for (let i = 1; i <= genButtons; ++i) {
         const button = document.createElement("button");
         button.type = "button";
-        button.innerText = `${i}`;
-        button.id = `${i}`;
+        button.innerText = i;
+        button.id = i;
         button.onclick= function displayMessage() {
             if (i === randomBtn) {
                 document.getElementById("Initial-text").innerHTML = "You win!";
